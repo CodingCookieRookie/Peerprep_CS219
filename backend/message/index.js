505a9f8cba13235c02836738f3c1d7b7f5bbbe6e
marcustw <e0014884@u.nus.edu.sg>
2021-10-11 18:01:08 +0800

    msg ms

---
+++ backend/message/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/chat/check', (req, res) => 
+  res.status(200).json({message: 'ok', data: 'Chat microservice is working!'}));
+
+io.on('connection', socket => {
+  console.log("A user connected");
+  socket.on('new-message', msg => {
+    console.log(msg);
+    io.emit(msg.sessionId, msg.message);
+  });
+});
+
+const port = process.env.PORT || 5002
+
+http.listen(port, () => {
+  console.log(`Message ms listening on port ${port}...`);
+});
