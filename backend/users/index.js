44cc7d99834cdd779924fa48b67f8813de14b830
nweiyue <weiyue16@gmail.com>
2021-10-17 19:20:19 +0800

    Add bodyPaser

--- backend/users/index.js
+++ backend/users/index.js
@@ -4,13 +4,20 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
+let bodyParser = require("body-parser");
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
 const dbUsername = process.env.DBUSERNAME;
 const dbPassword = process.env.DBPASSWORD;
 if (!dbUsername || !dbPassword) {
-  console.error(
-    "Please specify the database username and password as environment variables!"
-  );
-  process.exit(1);
+    console.error(
+        "Please specify the database username and password as environment variables!"
+    );
+    process.exit(1);
 }
 
 const uri = `mongodb+srv://team23:${dbPassword}@team23.77voc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -23,7 +30,7 @@ const userRouter = require("./routes/userRoutes");
 const profileRouter = require("./routes/profileRoutes");
 
 app.get("/api/user", (req, res) =>
-  res.status(200).json({ message: "User microservice is working!" })
+    res.status(200).json({ message: "User microservice is working!" })
 );
 app.use("/api", userRouter);
 app.use("/api", profileRouter);
@@ -31,5 +38,5 @@ app.use("/api", profileRouter);
 const port = process.env.PORT || 5001;
 
 app.listen(port, () =>
-  console.log(`Server listening at http://localhost:${port}`)
+    console.log(`Server listening at http://localhost:${port}`)
 );
