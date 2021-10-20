69aa16f8a32917a4d81f44677d102068ff8fa3da
DESKTOP-NS4HHOV\User <bioluminescencecloud@gmail.com>
2021-10-21 00:49:03 +0800

    Add relevant yaml files for each deployment and helm to deploy all

--- backend/match/index.js
+++ backend/match/index.js
@@ -41,7 +41,7 @@ else
     console.log("Db connected successfully")
 
 // Setup server port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5003;
 
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello welcome to peerprep!'));
