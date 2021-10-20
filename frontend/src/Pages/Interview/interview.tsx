c1ad00c26442fbd2d5e9b98010217e2600553498
marcustw <e0014884@u.nus.edu.sg>
2021-10-21 00:46:15 +0800

    change ports

--- frontend/src/Pages/Interview/interview.tsx
+++ frontend/src/Pages/Interview/interview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "@material-ui/core/Button";
 import Editor from "../../Components/Editor/editor";
 import Chat from "../../Components/Chat/chat";
