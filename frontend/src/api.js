6d944f100ee3f55a1c1d47d33367e4e587985eb7
marcustw <e0014884@u.nus.edu.sg>
2021-10-22 23:43:09 +0800

    missing /api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
 export const DEV_API_URL = "http://localhost:5001/api";
-export const PROD_API_URL = "https://users-6i7ougacoq-de.a.run.app"
+export const PROD_API_URL = "https://users-6i7ougacoq-de.a.run.app/api"
