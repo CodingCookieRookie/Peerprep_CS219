9604d1da528cb3c5471a72de8673dae3c5403ca7
Alvin <bioluminescencecloud@gmail.com>
2021-10-26 16:19:15 +0800

    Initialise values of new, put and delete

--- backend/match/matchController.js
+++ backend/match/matchController.js
@@ -20,20 +20,16 @@ exports.match = function (req, res) {
     });
 };
 
+/* Info on new: For each user, a match will be created with user's username, start xp = 0, with match = null, wantsMatch = false */
+/* Info on update: Update is called when click on match button -> wantsMatch status will be true -> If got match, match partner in both user's and match's match field will be updated */
+/* Info on delete: Match field on *CURRENT USER ONLY* will be null and wantsMatch status set to false */
+
 exports.new = function (req, res) {
     var match = new Match();
     match.username = req.body.username;
-    match.xp = req.body.xp;
-    match.match = req.body.match;
-    match.wantsMatch = req.body.wantsMatch;
-    console.log("user: " + match.username);
-    console.log("xp: " + match.xp);
-    console.log("wants: " + match.wantsMatch);
-    if (match.wantsMatch) {
-        console.log ("wants"); 
-    } else {
-        console.log("dont want");
-    }
+    match.xp = 0;
+    match.match = null;
+    match.wantsMatch = false;
     if (req.body.username == null) {
         res.json({
             status: "failed",
@@ -78,6 +74,7 @@ exports.update = function (req, res) {
         } else {
             const currentUserName = currentMatch.username;
             const currentUserXP = currentMatch.xp;
+            currentMatch.wantsMatch = true;
             Match.find({match: ""}, function (err, matches) {   // If have same xp
                 if (!matches || err) {
                     res.json({
@@ -118,10 +115,18 @@ exports.update = function (req, res) {
                         } 
                     }
                     if(!potentialExist) {
-                        res.json({
-                            status: "Fail",
-                            message: 'Cannot find a match',
-                            data: currentUserName
+                        currentMatch.save(function (err) {
+                            if (err) {
+                                res.status(400).json({
+                                    message: "Save error on current user wantMatch status: " + err.message,
+                                });
+                            } else {
+                                res.json({  // any res.json call should end the call
+                                    status: "Success",
+                                    message: 'Set current user wantMatch status to true successfully',
+                                    data: "Current user: " + currentUserName
+                                });
+                            }
                         });
                     }
                 }
@@ -145,7 +150,8 @@ exports.delete = function (req, res) {
                 data: match
             });
         } else {
-            match.match = "";
+            match.match = null;
+            match.wantsMatch = false;
             match.save(function (err) {
                 if (err) {
                     res.status(400).json({
