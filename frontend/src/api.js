a7205c452bb092333c047bd0029e56d2dd65c6c5
marcustw <e0014884@u.nus.edu.sg>
2021-10-28 23:32:38 +0800

    complete msging deployed endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 export const DEV_API_URL = "http://localhost:5001/api";
-export const DEV_MATCH_API_URL = "http://localhost:5003/api";
-export const DEV_MSG_API_URL = "https://message-6i7ougacoq-de.a.run.app";
+export const DEV_MATCH_API_URL = "http://localhost:5004/api";
+export const DEV_MSG_API_URL = "http://localhost:5002";
+export const DEV_EDITOR_API_URL = "http://localhost:5003";
 
 export const PROD_API_URL = "https://users-6i7ougacoq-de.a.run.app/api";
 export const PROD_MATCH_API_URL = "https://match-6i7ougacoq-de.a.run.app/api";
 export const PROD_MSG_API_URL = "https://message-6i7ougacoq-de.a.run.app";
+export const PROD_EDITOR_API_URL = "https://editor-6i7ougacoq-de.a.run.app";
