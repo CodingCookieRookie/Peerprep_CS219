d84d2f5bcc771fd31025f9750284bcfc2d8d29e9
MarcusTw <e0014884@u.nus.edu>
2021-10-29 20:22:00 +0800

    automation

---
+++ install-all.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd frontend && npm install
+cd ../backend/users && npm install
+cd ../../backend/editor && npm install
+cd ../../backend/match && npm install
+cd ../../backend/message && npm install
