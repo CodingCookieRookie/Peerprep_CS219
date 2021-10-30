19a0cea52acc009f04333f92db3b375d8156766d
MarcusTw <e0014884@u.nus.edu>
2021-10-30 16:52:58 +0800

    editor ms more edits

--- backend/editor/index.js
+++ backend/editor/index.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+
 const mongoose = require("mongoose");
 const Editor = require("./models/editorModel");
 
