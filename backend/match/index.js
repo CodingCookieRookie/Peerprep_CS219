b5b90b6b2ed169c76a33ac591ad0e0add9635e46
Alvin <bioluminescencecloud@gmail.com>
2021-11-09 16:22:59 +0800

    Change deploy cmd

--- backend/match/index.js
+++ backend/match/index.js
@@ -47,7 +47,7 @@ else
 var port = process.env.PORT || 5004;
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello and please welcome to peerprep!'));
+app.get('/', (req, res) => res.send('Match Microservice is running.'));
 
 // Use Api routes in the App
 app.use('/api', apiRoutes);
